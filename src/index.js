7eb59a46ccd44abffc516df309aa25e557a25c0e
Petro Svist <svist.petro@gmail.com>
2023-08-13 17:28:34 +0300

    Finaly

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import { doStuff } from './api-system';
+import { anserError, anserWarning, cleanerfotoTotalLength} from './notification';
+import { dissActiveBtnLoadMore } from './switch-button';
+import { clearPhotoContainerAndInputValue } from './cleaner';
+import { renderPhoto } from './renderihg-and-creating-gallery';
+export {refs, fotoTotal, page, processingPhoto };
+
+let page = 1;
+let searchText = '';
+let fotoTotal = 0;
+
+const refs = {
+  formSearch: document.querySelector('#search-form'),
+  input: document.querySelector('[type="text"]'),
+  btnSubmit: document.querySelector("[type='submit']"),
+  btnLoadMore: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+};
+refs.formSearch.addEventListener('submit', getTextForSearch);
+refs.btnLoadMore.addEventListener('click', searchPhoto);
+
+function getTextForSearch(event) {
+  event.preventDefault();
+  dissActiveBtnLoadMore();
+  searchText = event.currentTarget.elements.searchQuery.value.trim();
+  if (searchText === '') {
+    anserWarning();
+    return;
+  }
+  clearPhotoContainerAndInputValue();
+  page = 1;
+  cleanerfotoTotalLength();
+  scroll()
+  searchPhoto();
+}
+
+function searchPhoto() {
+  doStuff(searchText);
+}
+
+const processingPhoto = value => {
+  if (value.data.hits.length === 0) {
+    anserError();
+    return;
+  }
+  fotoTotal = value.data.totalHits;
+  renderPhoto(value.data);
+  page += 1;
+};
+
+
+
+const btnPort = document.getElementById('.gallery');
+
+function scroll(){
+    window.scrollBy({
+        //top: cardHeight * 2,
+        behavior: "smooth",
+      });
+}
+
+// let infScroll = new InfiniteScroll( elem, {
+//     // options
+//     path: '.pagination__next',
+//     append: '.post',
+//     history: false,
+//   });
+  
